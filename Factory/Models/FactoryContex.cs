87cdc6a0b162034fae8ab214a94e881a4e94281a
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-09 14:29:37 -0600

    adding database context code

---
+++ Factory/Models/FactoryContex.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+    public class FactoryContext : DbContext
+    {
+        public FactoryContext(DbContextOptions<FactoryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<EngineerMachine>()
+                .HasKey(em => new { em.EngineerId, em.MachineId });
+
+            builder.Entity<EngineerMachine>()
+                .HasOne(em => em.Engineer)
+                .WithMany(e => e.Machines)
+                .HasForeignKey(em => em.EngineerId);
+
+            builder.Entity<EngineerMachine>()
+                .HasOne(em => em.Machine)
+                .WithMany(m => m.Engineers)
+                .HasForeignKey(em => em.MachineId);
+        }
+
+        public DbSet<Engineer> Engineers { get; set; }
+        public DbSet<Machine> Machines { get; set; }
+        public DbSet<EngineerMachine> EngineerMachines { get; set; }
+    }
+}
