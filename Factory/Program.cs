b4fadae08199947c9fb17063603645875345fcd7
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-09 13:22:09 -0600

    add model files and update program.cs and appsetting.js to prepare migrations and set up

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -3,13 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Simplified EF Core with MySQL configuration
+builder.Services.AddDbContext<FactoryContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
