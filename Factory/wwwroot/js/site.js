1bb97bc1ff57c6209918cf297f19ead4b3e97775
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-25 22:06:19 -0600

    more accessiblity throughout project , and dark and light mode added

--- Factory/wwwroot/js/site.js
+++ Factory/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', () => {
+  const themeSwitcher = document.getElementById('theme-switcher');
+
+  //  saved theme preference
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+
+  themeSwitcher.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    let theme = 'light';
+    if (document.body.classList.contains('dark-mode')) {
+      theme = 'dark';
+    }
+    // theme change to locality 
+    localStorage.setItem('theme', theme);
+  });
+});
